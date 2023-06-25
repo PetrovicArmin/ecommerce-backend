1398a81bd52ec8793510e949d66664e77305f06f
Armin Petrović <apetrovic1@etf.unsa.ba>
2023-06-25 21:40:26 +0200

    NOT NULL change in product changes log

--- db_service/database_dump.sql
+++ db_service/database_dump.sql
@@ -114,7 +114,7 @@ CREATE TABLE product_changes_log (
 	id serial4 NOT NULL,
 	product_id int4 NOT NULL,
 	changed_by_user_id int4 NOT NULL,
-	change_type varchar NULL,
+	change_type varchar NOT NULL,
 	change_date_time timestamp NOT NULL,
 	CONSTRAINT product_changes_log_pk PRIMARY KEY (id)
 );
