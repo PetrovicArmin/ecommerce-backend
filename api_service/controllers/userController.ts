ff9fbfebca9cca4293a7f669794b42518727d576
Armin Petrović <apetrovic1@etf.unsa.ba>
2023-06-27 14:40:07 +0200

    Added deletion of user accounts

--- api_service/controllers/userController.ts
+++ api_service/controllers/userController.ts
@@ -101,5 +101,49 @@ export const updateUser: RequestHandler = async (req: Request, res: Response, ne
 }
 
 export const deleteUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const numberOfDestroyedRows = await PostgresDatabase.db.sq.transaction(async (t) => {
+
+            const destroyed = await PostgresDatabase.db.Users.destroy({
+                where: {
+                    id: req.params.id
+                },
+                transaction: t
+            });
     
+            await PostgresDatabase.db.ProductLogs.destroy({
+                where: {
+                    changedByUserId: req.params.id
+                },
+                transaction: t
+            });
+    
+            await PostgresDatabase.db.SkuLogs.destroy({
+                where: {
+                    changedByUserId: req.params.id
+                },
+                transaction: t
+            });
+    
+            await PostgresDatabase.db.InventoryLogs.destroy({
+                where: {
+                    changedByUserId: req.params.id
+                },
+                transaction: t
+            });
+
+            return destroyed;
+        })
+
+        if (numberOfDestroyedRows == 0) 
+            res.status(404).json({
+                message: `Resource 'user' with id of ${req.params.id} does not exist`
+            });
+        else 
+            res.status(200).json({
+                message: `Resource 'user' with id of ${req.params.id} successfully deleted`
+            });
+    } catch(err) {
+        res.status(400).json(err);
+    }
 }
