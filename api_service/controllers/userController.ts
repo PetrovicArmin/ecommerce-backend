a8a9cd8b0ab7b2ffbbf941f5e4f37d433337a54b
Armin Petrović <apetrovic1@etf.unsa.ba>
2023-06-27 14:30:27 +0200

    Added update API route for users. Tested it with postman

--- api_service/controllers/userController.ts
+++ api_service/controllers/userController.ts
@@ -72,7 +72,32 @@ export const readUser: RequestHandler = async (req: Request, res: Response, next
 }
 
 export const updateUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-    
+    try {
+        req.body.lastModified = new Date();
+
+        const result = await PostgresDatabase.db.Users.update(req.body, { 
+            where: { id: req.params.id }, 
+            returning: true
+        });
+
+        if (result[0] == 0) {
+            res.status(404).json({
+                message: `Resource 'user' with id of ${req.params.id} does not exist`
+            });
+            return;
+        }
+
+        const user: User = new User(result[1][0]);
+
+        res.setHeader('Last-Modified', user.lastModified.toUTCString());
+
+        res.status(200).json({
+            product: user.userResponse,
+            links: user.links
+        });
+    } catch(err) {
+        res.status(400).json(err);
+    }
 }
 
 export const deleteUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
