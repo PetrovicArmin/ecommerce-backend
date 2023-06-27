b99ea50231b8abbf8c61f7786a8e344399292949
Armin Petrović <apetrovic1@etf.unsa.ba>
2023-06-27 14:23:54 +0200

    Added getting one user and tested it

--- api_service/controllers/userController.ts
+++ api_service/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { RequestHandler, Request, Response, NextFunction } from "express";
 import { User } from "../models/users.js";
 import PostgresDatabase from "../database/postgresHandler.js";
+import isCached from "../middleware/chachingChecker.js";
 
 export const createUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -47,7 +48,27 @@ export const readUsers: RequestHandler = async (req: Request, res: Response, nex
 }
 
 export const readUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-    
+    try {
+        const instance: any = await PostgresDatabase.db.Users.findByPk(req.params.id)
+
+        if (instance == null) {
+            res.status(404).json({
+                message: `Resource 'user' with id of ${req.params.id} does not exist`
+            })
+            return;
+        }
+
+        const user: User = new User(instance);
+
+        if (!isCached(req, res, user.lastModified)) {
+            res.status(200).json({
+                user: user.userResponse,
+                links: user.links
+            });
+        }
+    } catch(err) {
+        res.status(400).json(err);
+    }
 }
 
 export const updateUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
